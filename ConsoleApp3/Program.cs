e605cf884add8bef857a23a1709b309e08cf7a18
zahrajafari <jafari_zhr@yahoo.com>
2023-04-15 21:59:34 +0330

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace project
+{
+    class test1
+    {
+        public static void Main()
+        {
+            Console.WriteLine("enter x");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Calc(x);
+        }//end main
+
+        private static void Calc(int x)
+        {
+            double Result = Math.Pow(x, 2) + (2 * x) - 4;
+            Console.WriteLine(Result);
+        }
+    }
+}
